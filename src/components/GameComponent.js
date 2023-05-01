1b6ec8046cf6aae5eea49958c472903403698871
Md Zaki Imam <zaki669@gmail.com>
2023-05-01 12:29:29 +0200

    alternate color of edge upon consecutive clicking

--- src/components/GameComponent.js
+++ src/components/GameComponent.js
@@ -1,50 +1,61 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import ForceGraph2D from 'react-force-graph-2d';
-// import "./styles.css";
 import { withResizeDetector } from 'react-resize-detector';
 
-// import treeNodeLinks from '../data/tree-node-links.json';
 import generateRandomTree from '../data/random-tree';
-// import testData from '../data/test-data.json';
-
-// const data = treeNodeLinks;
-const data = generateRandomTree(10, false);
-// const data = testData;
-
-data.nodes.forEach((node) => {
-  node.val = (node.size * 100) | (node.degrees / 10);
-  if (node.isClusterNode) {
-    node.clusterId = null;
-  } else {
-    const link = data.links.find((link) => link.target === node.id);
-    node.clusterId = link && link.source;
-  }
-});
-
-const Graph = (props) => {
-  console.log(data);
-  const { width = 0, height = 610 } = props;
-  const forceRef = useRef(null);
-  useEffect(() => {
-    forceRef.current.d3Force('charge').strength(-200);
-  });
-  console.log(width, height);
-
-  return (
-    <ForceGraph2D
-      graphData={data}
-      width={width}
-      height={height}
-      backgroundColor="aliceblue"
-      nodeLabel="id"
-      ref={forceRef}
-    />
-  );
-};
-
-const MyGraph = withResizeDetector(Graph);
 
 function GameComponent() {
+  const [data, setData] = useState(generateRandomTree(10, false));
+
+  console.log(data);
+
+  data.nodes.forEach((node) => {
+    node.val = (node.size * 100) | (node.degrees / 10);
+    if (node.isClusterNode) {
+      node.clusterId = null;
+    } else {
+      const link = data.links.find((link) => link.target === node.id);
+      node.clusterId = link && link.source;
+    }
+  });
+
+  const handleLinkClick = (link) => {
+    setData((prevData) => ({
+      ...prevData,
+      links: prevData.links.map((l) =>
+        l.source.id === link.source.id && l.target.id === link.target.id
+          ? l.color === 'red'
+            ? { ...l, color: '' }
+            : { ...l, color: 'red' }
+          : l
+      ),
+    }));
+  };
+
+  const Graph = (props) => {
+    const { width = 0, height = 600 } = props;
+    const forceRef = useRef(null);
+    useEffect(() => {
+      forceRef.current.d3Force('charge').strength(-200);
+    });
+
+    return (
+      <ForceGraph2D
+        graphData={data}
+        width={width}
+        height={height}
+        backgroundColor="aliceblue"
+        nodeLabel="id"
+        linkLabel="index"
+        ref={forceRef}
+        onLinkClick={handleLinkClick}
+        linkColor={(link) => link.color}
+      />
+    );
+  };
+
+  const MyGraph = withResizeDetector(Graph);
+
   return (
     <div
       style={{
