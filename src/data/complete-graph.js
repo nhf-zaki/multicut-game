55b039c48baa9bdaa501fca91035b0b3fe470fdd
Md Zaki Imam <zaki669@gmail.com>
2023-05-19 00:46:11 +0200

    graph data generation

--- src/data/complete-graph.js
+++ src/data/complete-graph.js
@@ -1,5 +1,16 @@
 function generateCompleteGraph(n = 20) {
-  const nodes = Array.from({ length: n }, (_, i) => ({ id: i }));
+  const radius = 100; // Radius of the circular layout
+  const centerX = 0; // X-coordinate of the center of the circle
+  const centerY = 0; // Y-coordinate of the center of the circle
+
+  const angleStep = (2 * Math.PI) / n; // Angle between each node in radians
+
+  const nodes = Array.from({ length: n }, (_, i) => ({
+    id: i,
+    x: centerX + radius * Math.cos(i * angleStep), // Calculate x position based on angle
+    y: centerY + radius * Math.sin(i * angleStep), // Calculate y position based on angle
+  }));
+
   const links = nodes.flatMap((node, i) =>
     nodes.slice(i + 1).map((target) => ({ source: node.id, target: target.id }))
   );
--- src/data/grid-graph.js
+++ src/data/grid-graph.js
@@ -1,13 +1,18 @@
 function generateGridGraph(n = 20) {
-  const nodes = Array.from({ length: n * n }, (_, i) => ({ id: i }));
+  const nodes = Array.from({ length: n * n }, (_, i) => ({
+    id: i,
+    x: i % n, // Calculate x position based on the column index
+    y: Math.floor(i / n), // Calculate y position based on the row index
+  }));
+
   const links = [];
 
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
-      if (i > 0) links.push({ source: i * n + j, target: (i - 1) * n + j });
-      if (j > 0) links.push({ source: i * n + j, target: i * n + j - 1 });
-      if (i < n - 1) links.push({ source: i * n + j, target: (i + 1) * n + j });
+      // Connect nodes horizontally
       if (j < n - 1) links.push({ source: i * n + j, target: i * n + j + 1 });
+      // Connect nodes vertically
+      if (i < n - 1) links.push({ source: i * n + j, target: (i + 1) * n + j });
     }
   }
 
